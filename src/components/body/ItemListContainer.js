cc69b6eacbad280af586c61a060086a01008c2f7
FredyPapa <fredy.luis.wolf@gmail.com>
2021-08-08 10:23:43 -0500

    Clase 5: Contador con Botón
    Clase 5: Contador con Botón

--- src/components/body/ItemListContainer.js
+++ src/components/body/ItemListContainer.js
@@ -11,24 +11,28 @@ function ItemListContainer() {
           titulo="Sitio Web - Pack 1"
           descripcion="Sitio Web responsive con Wordpress, Hasta 5 páginas internas, Dominio y Hosting por un año."
           costo="Costo: S/.500.00"
+          stock="10"
         />
         <ItemContainer
           imagen="https://elitesystems.mx/wp-content/uploads/2017/02/usa.png"
           titulo="Sitio Web - Pack 2"
           descripcion="Sitio Web responsive con Wordpress, Hasta 10 páginas internas, Dominio y Hosting por un año."
           costo="Costo: S/.700.00"
+          stock="8"
         />
         <ItemContainer
           imagen="https://www.easyappcode.com/images/page-desarrollo-de-aplicaciones-web.jpg"
           titulo="Sitio Web - Pack 3"
           descripcion="Sitio Web responsive con Wordpress, uso del plugin de maquetación Elementor Pro, Hasta 10 páginas internas, Dominio y Hosting por un año."
           costo="Costo: S/.1000.00"
+          stock="5"
         />
         <ItemContainer
-          imagen="https://www.comunicare.es/wp-content/uploads/2021/02/como-crear-pagina-web-mejores-programas-disponibles-730x485-977425330.png"
+          imagen="https://www.juangalera.com/wp-content/uploads/2017/03/sitio-web-importancia-imagen-destacada.png"
           titulo="Sitio Web - Pack 4"
           descripcion="Sitio Web responsive a medida con o sin Wordpress, uso del plugin de maquetación Elementor Pro (si opta por Wordpress), las páginas internas que necesite, Dominio y Hosting por un año."
           costo="Costo: S/.1,500.00"
+          stock="2"
         />
       </CardGroup>
     </div>
--- src/components/body/itemContainer.js
+++ src/components/body/itemContainer.js
@@ -1,20 +1,31 @@
 import React from "react";
 import { Card } from "react-bootstrap";
+import { useState } from "react";
+import ItemCount from "./ItemCount";
 
-function ItemContainer({imagen, titulo, descripcion, costo}) {
+function ItemContainer({ imagen, titulo, descripcion, costo, stock }) {
+  //
+  const [stockProducto, setStockProducto] = useState(stock);
+  //
+  const actualizarStock = (nuevoStock)=>{
+    setStockProducto(nuevoStock);
+  };
+  //
   return (
     <div className="margenesContenido">
-      <Card className="anchoCard">
+      <Card className="estiloCard">
         <Card.Img variant="top" src={imagen} />
+        <Card.Header className="headerCard">
+          <small>Servicios disponibles para este mes: {stockProducto}</small>
+        </Card.Header>
         <Card.Body>
           <Card.Title className="comprar">{titulo}</Card.Title>
-          <Card.Text>
-            {descripcion}
-          </Card.Text>
+          <Card.Text>{descripcion}</Card.Text>
         </Card.Body>
-        <Card.Footer>
+        <Card.Footer className="footerCard">
           <small className="text-muted">{costo}</small>
         </Card.Footer>
+        <ItemCount stock={stockProducto} handler={actualizarStock} />
       </Card>
     </div>
   );
