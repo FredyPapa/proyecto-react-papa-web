107b74eb1b9d739a8fa5aa601ab185a2ec98a8ce
FredyPapa <fredy.luis.wolf@gmail.com>
2021-08-18 18:34:10 -0500

    Clase 08: Primera Entrega del Proyecto Final
    Clase 08: Primera Entrega del Proyecto Final

--- src/components/BodyContainer/index.js
+++
@@ -1,9 +0,0 @@
-import ItemListContainer from "./ItemListContainer";
-
-export default function BodyContainer(){
-    return(
-        <div>
-            <ItemListContainer/>
-        </div>
-    );
-}
--- src/components/HeaderContainer/index.js
+++
@@ -1,9 +0,0 @@
-import "./NavBar";
-import NavBar from "./NavBar";
-export default function HeaderContainer(){
-    return(
-        <div>
-            <NavBar/>
-        </div>
-    );
-}
