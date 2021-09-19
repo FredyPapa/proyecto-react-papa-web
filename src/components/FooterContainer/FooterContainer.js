27247a56cd10f9cdfca027539aac14c426958f16
FredyPapa <fredy.luis.wolf@gmail.com>
2021-09-19 16:02:38 -0500

    Proyecto Final

--- src/components/FooterContainer/FooterContainer.js
+++
@@ -1,8 +0,0 @@
-const FooterContainer = () =>{
-    return(
-        <footer className="detalleFooter">
-            <p className="detalleFooter__texto">© Todos los derechos reservados</p>
-        </footer>
-    );
-}
-export default FooterContainer;
